b5b57167c0ab36f650f673494f731b022cc282ae
Matthew Jackson <mattshiloh@gmail.com>
2018-09-18 19:47:52 -0500

    built cards

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log
+const toDoInputElem = document.getElementById('toDoInput');
+const notesInputElem = document.getElementById('notesInput');
+
+const submitToDoButton = document.getElementById('submitToDoButton');
+
+const buildNewToDoCard = (toDo, notes) => {
+    let domString = `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">${toDo}</h5>
+        <p class="card-text">${notes}</p>
+        <a href="#" class="btn btn-primary">Go Somewhere</a>
+    </div>
+    </div>`;
+
+    printToDom(domString, 'toDoCards');
+}
+
+const printToDom = (stringToPrint, whereToPrint) => {
+        document.getElementById(whereToPrint).innerHTML += stringToPrint;
+}
+
+
+
+
+submitToDoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    buildNewToDoCard(toDoInputElem.value, notesInputElem.value);
+});
