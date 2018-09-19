153fcaa39124721c7a294f4708fbe62ab677d88d
Matthew Jackson <mattshiloh@gmail.com>
2018-09-18 20:01:33 -0500

    event listeners

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const notesInputElem = document.getElementById('notesInput');
 
 const submitToDoButton = document.getElementById('submitToDoButton');
 
+const activateDeletes = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+    console.log(deleteButtons);
+
+    for (let i = 0; i < deleteButtons.length; i++){
+        const element = deleteButtons[i];
+        element.addEventListener("click", () => {
+            console.log("they clicked delete!!!");
+        })
+    }
+}
+
 const buildNewToDoCard = (toDo, notes) => {
     let domString = `<div class="card" style="width: 18rem;">
     <div class="card-body">
         <h5 class="card-title">${toDo}</h5>
         <p class="card-text">${notes}</p>
-        <a href="#" class="btn btn-primary">Go Somewhere</a>
+        <button href="#" class="btn btn-primary deletedButton">Delete this Shit</button>
     </div>
     </div>`;
 
